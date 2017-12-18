a7979b64d5ff98a51cdda8ff4b720c8cc336c84c
listencx <newchenxin@qq.com>
2017-12-18 10:03:14 +0800

    add websocket

---
+++ src/main/java/com/example/demo/domain/RequestMessage.java
@@ -0,0 +1,9 @@
+package com.example.demo.domain;
+
+public class RequestMessage {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+}
--- src/main/java/com/example/demo/websocket/MyWebsocket.java
+++ src/main/java/com/example/demo/websocket/MyWebsocket.java
@@ -1,5 +1,100 @@
 package com.example.demo.websocket;
 
-public class MyWebsocket {
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArraySet;
 
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.stereotype.Component;
+
+@ServerEndpoint(value = "/websocket")
+@Component
+public class MyWebsocket {
+    //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
+    private static int onlineCount = 0;
+
+    //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebsocket对象。
+    private static CopyOnWriteArraySet<MyWebsocket> webSocketSet = new CopyOnWriteArraySet<MyWebsocket>();
+
+    //与某个客户端的连接会话，需要通过它来给客户端发送数据
+    private Session session;
+
+    /**
+     * 连接建立成功调用的方法*/
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+        webSocketSet.add(this);     //加入set中
+        addOnlineCount();           //在线数加1
+        try {
+        	sendInfo("有新连接加入！当前在线人数为" + getOnlineCount());
+		} catch (IOException e1) {
+			System.out.println("IO异常");
+		}
+        System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
+    }
+
+    /**
+     * 连接关闭调用的方法
+     * @throws IOException 
+     */
+    @OnClose
+    public void onClose() throws IOException {
+        webSocketSet.remove(this);  //从set中删除
+        subOnlineCount();           //在线数减1
+        sendInfo("有一连接关闭！当前在线人数为" + getOnlineCount());
+        System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
+    }
+
+    /**
+     * 收到客户端消息后调用的方法
+     *
+     * @param message 客户端发送过来的消息
+     * @throws IOException */
+    @OnMessage
+    public void onMessage(String message, Session session) throws IOException {
+        System.out.println("来自客户端的消息:" + message);
+        //群发消息
+        sendInfo(message);
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        System.out.println("发生错误");
+        error.printStackTrace();
+    }
+
+
+    public void sendMessage(String message) throws IOException {
+        this.session.getBasicRemote().sendText(message);
+        //this.session.getAsyncRemote().sendText(message);
+    }
+
+
+    public static void sendInfo(String message) throws IOException {
+        for (MyWebsocket item : webSocketSet) {
+            try {
+                item.sendMessage(message);
+            } catch (IOException e) {
+                continue;
+            }
+        }
+    }
+
+    public static synchronized int getOnlineCount() {
+        return onlineCount;
+    }
+
+    public static synchronized void addOnlineCount() {
+        MyWebsocket.onlineCount++;
+    }
+
+    public static synchronized void subOnlineCount() {
+        MyWebsocket.onlineCount--;
+    }
 }
